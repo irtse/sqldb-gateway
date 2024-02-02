5b2a4db20ede48d54bed29ea751a74ad0fe193ca
YaDesMarqueurs <morgane.roques@irt-saintexupery.com>
2024-02-02 18:03:13 +0100

    security description

--- lib/params.go
+++ lib/params.go
@@ -19,8 +19,8 @@ var RootParamsDesc = map[string]string{
 	RootRowsParam : "needed on a rows level request (value=all for post/put method or a get/delete all)",
     RootColumnsParam : "needed on a columns level request (POST/PUT/DELETE with no rows query params) will set up a view on row level (show only expected columns)",
 	RootShallow : "activate a lightest response (name only)",
-	RootOrderParam : "set 'enable' sets up a sql order in query",
-	RootDirParam : "sets up a sql direction in query (ex.ASC)",
+	RootOrderParam : "sets up a sql order in query (don't even try to inject you little joker)",
+	RootDirParam : "sets up a sql direction in query (ex.ASC|DESC) (don't even try to inject you little joker)",
 	RootRawView : "set 'enable' to activate a response without the main response format (only available if super admin)",
 }
 var HiddenParams = []string{RootDestTableIDParam}
