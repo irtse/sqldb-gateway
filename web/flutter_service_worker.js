3f6d79e2a72710150aeadc32a31a4c6f2d67b82a
YaDesMarqueurs <morgane.roques@irt-saintexupery.com>
2024-05-29 14:37:43 +0200

    New version

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "1c924d6951adca9a6acd50cf9608e8a0",
+const RESOURCES = {"main.dart.js": "46d7cf1422655f806417d67d0419c5e3",
 "manifest.json": "7ce3ab057b2d2f5e46534f82455de744",
 "assets/AssetManifest.json": "fa3f8d29b0a9eaaee7041c4919a652b3",
 "assets/assets/images/slide2.png": "3439248731f35a0b6dce06d6d0c61d80",
@@ -295,8 +295,8 @@ const RESOURCES = {"main.dart.js": "1c924d6951adca9a6acd50cf9608e8a0",
 "assets/packages/sign_in_button/assets/logos/google_light.png": "f71e2d0b0a2bc7d1d8ab757194a02cac",
 "assets/FontManifest.json": "1d1b2da5c95cb18a543e121865df72ff",
 "assets/AssetManifest.bin": "2050b573302b4cf758b4cd3789b0b39d",
-"index.html": "2a6df6a3d9f33f5bc068c06e768abbf4",
-"/": "2a6df6a3d9f33f5bc068c06e768abbf4",
+"index.html": "cb0cec61a7907c1708167ceee348c277",
+"/": "cb0cec61a7907c1708167ceee348c277",
 "favicon.png": "7acd80e0ce19e8cfc8ab84e17a5609ce",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
