5ba9b90372d121b23fc8be2ceca2c3688617743d
mr <morgane.roques@irt-saintexupery.com>
2025-07-02 14:04:32 +0200

    view

--- domain/domain_service/view_convertor/view_convertor.go
+++ domain/domain_service/view_convertor/view_convertor.go
@@ -526,16 +526,20 @@ func IsReadonly(tableName string, record utils.Record, createdIds []string, d ut
 		} else {
 			m[ds.DestTableDBField] = record[utils.SpecialIDParam]
 			m[ds.SchemaDBField] = sch.ID
+			m["is_close"] = true
+			fmt.Println("THA", tableName)
+			if res, err := d.GetDb().ClearQueryFilter().SelectQueryWithRestriction(ds.DBRequest.Name, m, false); err == nil && len(res) > 0 {
+				fmt.Println("THA2", tableName)
+				return true
+			}
+			m["is_close"] = false
 			if res, err := d.GetDb().ClearQueryFilter().SelectQueryWithRestriction(ds.DBRequest.Name, m, false); err != nil || len(res) == 0 {
 				for k, _ := range d.GetParams().Values {
 					if sch.HasField(k) {
 						return true
 					}
 				}
-				m["is_close"] = true
-				if res, err := d.GetDb().ClearQueryFilter().SelectQueryWithRestriction(ds.DBRequest.Name, m, false); err == nil && len(res) > 0 {
-					return true
-				}
+
 				for _, f := range sch.Fields {
 					if f.GetLink() > 0 && !strings.Contains(f.Type, "many") {
 						if sch2, err := scheme.GetSchemaByID(f.GetLink()); err == nil {
