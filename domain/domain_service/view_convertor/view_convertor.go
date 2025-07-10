f50f9f5484c9a22410fcd8fa6c3b2713e7ae9426
mr <morgane.roques@irt-saintexupery.com>
2025-07-10 16:11:50 +0200

    test

--- domain/domain_service/view_convertor/view_convertor.go
+++ domain/domain_service/view_convertor/view_convertor.go
@@ -49,6 +49,7 @@ func (v *ViewConvertor) TransformToView(results utils.Results, tableName string,
 
 func (v *ViewConvertor) transformFullView(results utils.Results, schema *sm.SchemaModel, isWorkflow bool, params utils.Params) utils.Results {
 	schemes, id, order, _, addAction, _ := v.GetViewFields(schema.Name, false, results)
+	fmt.Println("ORD", order)
 	commentBody := map[string]interface{}{}
 	if len(results) == 1 {
 		commentBody = map[string]interface{}{
@@ -143,7 +144,7 @@ func (v *ViewConvertor) transformShallowedView(results utils.Results, tableName 
 		}
 	}
 	scheme, id, order, _, addAction, _ := v.GetViewFields(t, false, utils.Results{})
-
+	fmt.Println("qshfk", order)
 	for _, record := range results {
 		if _, ok := record["is_draft"]; ok && record.GetBool("is_draft") && !v.Domain.IsOwn(false, false, utils.SELECT) {
 			continue
