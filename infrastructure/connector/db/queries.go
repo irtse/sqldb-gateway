8f94c01d01aa812c58907e52e85f74ae152a847d
mr <morgane.roques@irt-saintexupery.com>
2025-07-21 10:06:33 +0200

    test

--- infrastructure/connector/db/queries.go
+++ infrastructure/connector/db/queries.go
@@ -34,9 +34,6 @@ func (db *Database) SelectQueryWithRestriction(name string, restrictions interfa
 	}
 
 	res, err := db.QueryAssociativeArray(q)
-	if name == "dbworkflow" {
-		fmt.Println(q, err)
-	}
 	return res, err
 }
 
