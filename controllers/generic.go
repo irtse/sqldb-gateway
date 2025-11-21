0c36e2cb148f2d83e66391bf45c1c0e78bc2fd1e
mr <morgane.roques@irt-saintexupery.com>
2025-11-21 17:18:53 +0100

    new version of validation

--- controllers/generic.go
+++ controllers/generic.go
@@ -27,7 +27,8 @@ func (t *GenericController) GetMessage() {
 		host = "redis-server:6379"
 	}
 	fmt.Println("racac", host, code)
-	var s = "false"
+	var mess = ""
+	var ok = "false"
 	path := strings.Split(t.Ctx.Input.URI(), "?")
 	fmt.Println("racac1", path)
 	if len(path) >= 2 {
@@ -35,19 +36,33 @@ func (t *GenericController) GetMessage() {
 		for _, val := range uri {
 			kv := strings.Split(val, "=")
 			if len(kv) > 1 && kv[0] == "message" {
-				s = kv[1]
-				break
+				mess = kv[1]
+			}
+			if len(kv) > 1 && kv[0] == "got_response" {
+				ok = kv[1]
 			}
 		}
 	}
-	fmt.Println("racac4", path)
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     host, // Redis server address
 		Password: "",   // no password set
 		DB:       0,    // use default DB
 	})
+	go func() {
+		time.Sleep(10 * time.Minute)
+		rdb.Set(context.Background(), code+"_block", "blocked", 730*time.Hour)
+	}()
 	// Save data to Redis
-	if err := rdb.Set(context.Background(), code+"_str", s, 24*time.Hour).Err(); err != nil {
+	if err := rdb.Set(context.Background(), code, ok, 24*time.Hour).Err(); err != nil {
+		fmt.Println("Could not set key: %v", err)
+		t.Data["json"] = map[string]interface{}{
+			"status": "NOT OK",
+			"error":  err,
+		}
+		t.ServeJSON()
+		return
+	}
+	if err := rdb.Set(context.Background(), code+"_str", mess, 24*time.Hour).Err(); err != nil {
 		fmt.Println("Could not set key: %v", err)
 		t.Data["json"] = map[string]interface{}{
 			"status": "NOT OK",
@@ -96,29 +111,32 @@ func (t *GenericController) GetOK() {
 			}
 		}
 	}
-	fmt.Println("racac4", path)
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     host, // Redis server address
 		Password: "",   // no password set
 		DB:       0,    // use default DB
 	})
-	// Save data to Redis
-	if err := rdb.Set(context.Background(), code, s, 25*time.Hour).Err(); err != nil {
-		fmt.Println("Could not set key: %v", err)
-		t.Data["json"] = map[string]interface{}{
-			"status": "NOT OK",
-			"error":  err,
+	target := os.Getenv("LANG")
+	t.Ctx.Output.ContentType("text/html") // Optional, Beego usually handles it
+	if val, err := rdb.Get(context.Background(), code+"_block").Result(); err == nil && val != "" {
+		if target == "" {
+			target = "fr"
 		}
-		t.ServeJSON()
+		f, err := os.ReadFile("/opt/html/index_" + target + "_block.html")
+		if err != nil {
+			t.Data["error"] = err
+		}
+		content := string(f)
+		content = strings.ReplaceAll(content, "<code>", code)
+
+		t.Ctx.WriteString(content)
 		return
 	}
-	t.Ctx.Output.ContentType("text/html") // Optional, Beego usually handles it
-	target := os.Getenv("LANG")
 
 	if target == "" {
 		target = "fr"
 	}
-	f, err := os.ReadFile("/opt/html/index_" + target + ".html")
+	f, err := os.ReadFile("/opt/html/index_" + target + "_" + s + ".html")
 	if err != nil {
 		t.Data["error"] = err
 	}
@@ -157,6 +175,9 @@ func (t *GenericController) Get() {
 	fmt.Println("KEYZ", keys)
 	data := map[string]string{}
 	for _, key := range keys {
+		if strings.Contains(key, "_block") {
+			continue
+		}
 		val, err := rdb.Get(context.Background(), key).Result()
 		fmt.Println(key, val, err)
 		if err == nil {
